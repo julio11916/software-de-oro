e7a875413f67710903c356477d2d02495f269172
Jilmer Otalvaro <jilmer2006augusto@gmail.com>
2026-03-03 15:30:19 -0500

    ajustes dashboard promociones

--- static/js/administrador/admin_dashboard_principal.js
+++ static/js/administrador/admin_dashboard_principal.js
@@ -21,3 +21,19 @@ function actualizarFechaHora() {
 
 setInterval(actualizarFechaHora, 1000);
 actualizarFechaHora();
+// Manejo del botón Volver
+document.getElementById('backBtn').addEventListener('click', function () {
+    if (window.history.length > 1) {
+        window.history.back();
+    } else {
+        window.location.href = '{{ url_for("home") }}';
+    }
+});
+
+function fadeAndRedirect(event, url) {
+    event.preventDefault();
+    document.body.classList.add("fade-out");
+    setTimeout(() => {
+        window.location.href = url;
+    }, 300);
+}
